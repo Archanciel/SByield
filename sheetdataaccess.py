3da832c607b15ddcc169e604a1238954641b332e
Archanciel <jp.schnyder@gmail.com>
2021-03-11 01:58:05 +0100

    Yield daily rate computation ok. all tsts ok

--- sheetdataaccess.py
+++ sheetdataaccess.py
@@ -1,31 +1,76 @@
 import numpy as np
 import pandas as pd
 
-SB_ACCOUNT_SHEET_FIAT = 'USD'
-SB_ACCOUNT_SHEET_NAME = 'Transactions'
-SB_ACCOUNT_SHEET_SKIP_ROWS = 8
-SB_ACCOUNT_SHEET_HEADER_DATE = 'Local time'
-SB_ACCOUNT_SHEET_HEADER_EARNING = 'Net amount ({})'.format(SB_ACCOUNT_SHEET_FIAT)
-SB_ACCOUNT_SHEET_HEADER_TYPE = 'Type'
-SB_ACCOUNT_SHEET_HEADER_CURRENCY = 'Currency'
+# Swissborg account statement sheet parameters
+SB_ACCOUNT_SHEET_FIAT = 'USD'           # earning fiat: USD or CHF as defined when downloading the account statement Excel sheet
+SB_ACCOUNT_SHEET_NAME = 'Transactions'  # name of the spreadsheet
+SB_ACCOUNT_SHEET_SKIP_ROWS = 8          # number of lines above the column headers to skip
+
+# Swissborg account statement sheet column headers
+SB_ACCOUNT_SHEET_HEADER_DATE = 'Local time'     # yyyy-mm-dd hh:mm
+SB_ACCOUNT_SHEET_HEADER_EARNING = 'Net amount'  # column containing the daily earning in USDC or CHSB for example
+SB_ACCOUNT_SHEET_HEADER_TYPE = 'Type'           # row type: Earnings or Deposit for example
+SB_ACCOUNT_SHEET_HEADER_CURRENCY = 'Currency'   # USDC or CHSB for example
+
+# Swissborg account statement sheet columns to remove
 SB_ACCOUNT_SHEET_UNUSED_COLUMNS_LIST = ['Time in UTC',
                                         'Fee',
                                         'Fee ({})'.format(SB_ACCOUNT_SHEET_FIAT),
-                                        'Gross amount',
                                         'Gross amount ({})'.format(SB_ACCOUNT_SHEET_FIAT),
+                                        'Gross amount',
+                                        'Net amount ({})'.format(SB_ACCOUNT_SHEET_FIAT),
                                         'Note']
 
-SB_ACCOUNT_SHEET_HEADER_CAPITAL = 'CAPITAL'
-SB_ACCOUNT_SHEET_HEADER_BUY_SELL = 'BUY/SELL'
+# two new columns to add to the Swissborg account statement sheet
+SB_ACCOUNT_SHEET_HEADER_EARNING_CAPITAL = 'EARNING CAP'
+SB_ACCOUNT_SHEET_HEADER_DEPOSIT_WITHDRAW = 'DEP/WITHDR'
 
-SB_ACCOUNT_SHEET_TYPE_EARNING = 'Earnings'
-SB_ACCOUNT_SHEET_CURRENCY = 'USDC'
+SB_ACCOUNT_SHEET_TYPE_EARNING = 'Earnings'  # used to filter rows
+SB_ACCOUNT_SHEET_CURRENCY = 'USDC'          # used to filter rows. USDC or CHSB
+
+# Deposit/Withdrawal sheet parameters
+DEPOSIT_SHEET_HEADER_DATE = SB_ACCOUNT_SHEET_HEADER_DATE
+DEPOSIT_SHEET_HEADER_DEPOSIT_WITHDRAW = SB_ACCOUNT_SHEET_HEADER_DEPOSIT_WITHDRAW
+
+# Swissborg account statement merged with deposit/withdrawal sheet parameters
+MERGED_SHEET_HEADER_DATE = SB_ACCOUNT_SHEET_HEADER_DATE
+MERGED_SHEET_HEADER_INDEX = 'IDX'
+MERGED_SHEET_HEADER_EARNING_CAPITAL = SB_ACCOUNT_SHEET_HEADER_EARNING_CAPITAL
+MERGED_SHEET_HEADER_DEPOSIT_WITHDRAW = DEPOSIT_SHEET_HEADER_DEPOSIT_WITHDRAW
+MERGED_SHEET_HEADER_EARNING = SB_ACCOUNT_SHEET_HEADER_EARNING
+MERGED_SHEET_HEADER_DATE_NEW_NAME = 'DATE'
+MERGED_SHEET_HEADER_EARNING_NEW_NAME = 'EARNING'
+MERGED_SHEET_HEADER_YIELD_RATE = 'YIELD RATE'
+
+# columns which can be removed from the merged data frame
+MERGED_SHEET_UNUSED_COLUMNS_LIST = [SB_ACCOUNT_SHEET_HEADER_TYPE,
+                                    SB_ACCOUNT_SHEET_HEADER_CURRENCY]
 
 class SheetDataAccess:
+	"""
+	This class loads the Swissborg account statement xlsl sheet and the Deposit/Withdrawal
+	csv files.
+	
+	It merges the two files, creating a Pandas data frame containing computed data used
+	later to distribute the earnings according to the deposits/withdrawals.
+	"""
 	def __init__(self, configMgr):
+		"""
+		Currently, the configMgr is not used. Constants are used in place.
+		
+		:param configMgr:
+		"""
 		self.configMgr = configMgr
 		
 	def loadSBEarningSheet(self, sbAccountSheetFilePathName):
+		"""
+		Creates a Pandas data frame from the Swissborg account statement sheet, removing
+		unused columns. It then selects only the earning type rows. Finally, it adds
+		two new columns for later usage.
+		
+		:param sbAccountSheetFilePathName:
+		:return:
+		"""
 		xls = pd.ExcelFile(sbAccountSheetFilePathName,
 						   engine='openpyxl') # this parm is required on Android !
 		
@@ -43,8 +88,68 @@ class SheetDataAccess:
 		sbEarningsDf = sbEarningsDf[isTypeEarning & isTargetCurrency]
 		
 		# inserting two empty columns
-		sbEarningsDf.insert(loc=0, column=SB_ACCOUNT_SHEET_HEADER_CAPITAL, value=[0.0 for i in range(sbEarningsDf.shape[0])])
-		sbEarningsDf.insert(loc=0, column=SB_ACCOUNT_SHEET_HEADER_BUY_SELL, value=[0.0 for i in range(sbEarningsDf.shape[0])])
-
+		sbEarningsDf.insert(loc=0, column=SB_ACCOUNT_SHEET_HEADER_EARNING_CAPITAL, value=[0.0 for i in range(sbEarningsDf.shape[0])])
+		sbEarningsDf.insert(loc=0, column=SB_ACCOUNT_SHEET_HEADER_DEPOSIT_WITHDRAW, value=[0.0 for i in range(sbEarningsDf.shape[0])])
 		
 		return sbEarningsDf
+	
+	def loadDepositSheet(self, sbDepositSheetFilePathName):
+		"""
+		Creates a Pandas data frame from the Deposit/Withdrawal sheet.
+		
+		:param sbDepositSheetFilePathName:
+		:return:
+		"""
+		depositsDf = pd.read_csv(sbDepositSheetFilePathName, parse_dates=[DEPOSIT_SHEET_HEADER_DATE], dtype={DEPOSIT_SHEET_HEADER_DEPOSIT_WITHDRAW: np.float64})
+		depositsDf = depositsDf.set_index([DEPOSIT_SHEET_HEADER_DATE])
+		
+		return depositsDf
+
+	def mergeEarningAndDeposit(self, earningDf, depositDf):
+		"""
+		Merges Deposit/Withdrawal dataframe into the Swissborg account statement dataframe.
+		Then computes the earning capital values aswell as the daily yield rates.
+		
+		:param earningDf:
+		:param depositDf:
+		:return:
+		"""
+		# appending depositDf to earningDf, then sorting and converting NaN to zero
+		mergedDf = earningDf.append(depositDf).sort_index().fillna(0)
+
+		# drop unused columns
+		mergedDf = mergedDf.drop(columns=MERGED_SHEET_UNUSED_COLUMNS_LIST)
+
+		# replace DATE index with integer index
+		
+			# save old DATE index Column
+		mergedDf[MERGED_SHEET_HEADER_DATE] = mergedDf.index
+		
+			# reposition the DATE column to the left of the data frame
+		cols = mergedDf.columns.to_list()
+		cols = cols[-1:] + cols[:-1]
+		mergedDf = mergedDf[cols]
+		
+		# set integer index
+		mergedDf[MERGED_SHEET_HEADER_INDEX] = range(1, len(mergedDf) + 1)
+		mergedDf = mergedDf.set_index(MERGED_SHEET_HEADER_INDEX)
+
+		# adding yield column
+		mergedDf.insert(loc=mergedDf.shape[1], column=MERGED_SHEET_HEADER_YIELD_RATE, value=[0.0 for i in range(mergedDf.shape[0])])
+
+		# computing the earning capital value as well as the daily yield rate
+		for i in range(2, len(mergedDf) + 1):
+			mergedDf.loc[i, MERGED_SHEET_HEADER_EARNING_CAPITAL] = mergedDf.loc[i - 1, MERGED_SHEET_HEADER_EARNING_CAPITAL] + \
+			                                                       mergedDf.loc[i - 1, MERGED_SHEET_HEADER_DEPOSIT_WITHDRAW] + \
+			                                                       mergedDf.loc[i - 1, MERGED_SHEET_HEADER_EARNING]
+			earningCapital = mergedDf.loc[i, MERGED_SHEET_HEADER_EARNING_CAPITAL]
+			earning = mergedDf.loc[i, MERGED_SHEET_HEADER_EARNING]
+			
+			if earningCapital > 0 and earning > 0:
+				mergedDf.loc[i, MERGED_SHEET_HEADER_YIELD_RATE] = 1 + \
+				                                                  earning / \
+				                                                  earningCapital
+				
+		mergedDf = mergedDf.rename(columns={MERGED_SHEET_HEADER_DATE: MERGED_SHEET_HEADER_DATE_NEW_NAME, MERGED_SHEET_HEADER_EARNING: MERGED_SHEET_HEADER_EARNING_NEW_NAME})
+		
+		return mergedDf
--- test/testsheetdataaccess.py
+++ test/testsheetdataaccess.py
@@ -29,8 +29,47 @@ class TestSheetDataAccess(unittest.TestCase):
 			sbAccountSheetFilePathName = 'D:\\Development\\Python\\SByield\\test\\testData\\' + sbAccountSheetFileName
 
 		sbEarningsDf = self.sheetDataAccess.loadSBEarningSheet(sbAccountSheetFilePathName)
-		self.assertEqual((3, 6), sbEarningsDf.shape)
-		#print(sbEarningsDf.info())
-		#print(sbEarningsDf)
+		self.assertEqual((9, 5), sbEarningsDf.shape)
+		print(sbEarningsDf.info())
+		print(sbEarningsDf)
+
+	def testLoadDepositSheet(self):
+		depositSheetFileName = 'testDepositUsdc.csv'
+
+		if os.name == 'posix':
+			depositSheetFilePathName = '/storage/emulated/0/Android/data/ru.iiec.pydroid3/files/SByield/test/testdata/' + depositSheetFileName
+		else:
+			depositSheetFilePathName = 'D:\\Development\\Python\\SByield\\test\\testData\\' + depositSheetFileName
+
+		depositDf = self.sheetDataAccess.loadDepositSheet(depositSheetFilePathName)
+		self.assertEqual((3, 1), depositDf.shape)
+		print(depositDf.info())
+		print(depositDf)
+		
+	def testMergeEarningAndDeposit(self):
+		sbAccountSheetFileName = 'testSBEarningUsdc.xlsx'
+		depositSheetFileName = 'testDepositUsdc.csv'
+
+		if os.name == 'posix':
+			sbAccountSheetFilePathName = '/storage/emulated/0/Android/data/ru.iiec.pydroid3/files/SByield/test/testdata/' + sbAccountSheetFileName
+			depositSheetFilePathName = '/storage/emulated/0/Android/data/ru.iiec.pydroid3/files/SByield/test/testdata/' + depositSheetFileName
+		else:
+			sbAccountSheetFilePathName = 'D:\\Development\\Python\\SByield\\test\\testData\\' + sbAccountSheetFileName
+			depositSheetFilePathName = 'D:\\Development\\Python\\SByield\\test\\testData\\' + depositSheetFileName
+
+		sbEarningsDf = self.sheetDataAccess.loadSBEarningSheet(sbAccountSheetFilePathName)
+		depositDf = self.sheetDataAccess.loadDepositSheet(depositSheetFilePathName)
+		
+		mergedEarningDeposit = self.sheetDataAccess.mergeEarningAndDeposit(sbEarningsDf, depositDf)
+		self.assertEqual((12, 5), mergedEarningDeposit.shape)
+
+		print(mergedEarningDeposit.info())
+		print(mergedEarningDeposit)
+
 if __name__ == '__main__':
-    unittest.main()
+#    unittest.main()
+	tst = TestSheetDataAccess()
+	tst.setUp()
+#	tst.testLoadSBEarningSheet()
+	tst.setUp()
+	tst.testMergeEarningAndDeposit()
