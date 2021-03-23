9b229ce5b6761544b5c45eb0ff1e34a76c88e812
Archanciel <jp.schnyder@gmail.com>
2021-03-23 23:18:26 +0100

    handling  withdrawal correctl

--- test/swissborg_test_yields_generator.py
+++ test/swissborg_test_yields_generator.py
@@ -6,14 +6,18 @@ dayNumber = 5
 # generating day date list
 dayDates = pd.date_range("2021-01-01", periods=dayNumber, freq="D")
 
+depWithdrArray = [0.0] * dayNumber
 capitalArray = [0.0] * dayNumber
-capitalArray[0] = 19571.69
+
+depWithdrArray[0] = 19571.69
+depWithdrArray[2] = -19581.69
 
 zeroYieldArray = [0.0] * dayNumber
 
 DATE = 'DATE'
 RATE = 'RATE'
-CAPITAL = 'CAPITAL'
+DEPWITHDR = 'DEP/WITHDR'
+CAPITAL = 'EARNING CAPITAL'
 YIELD = 'YIELD'
 TOTAL = 'TOTAL'
 
@@ -21,13 +25,25 @@ lastRowIdx = dayNumber - 1
 
 def computeYields(df):
 	for i in range(0, dayNumber):
-		capital = df.loc[i, 'CAPITAL']
-		capitalPlusYield = capital * df.loc[i, 'RATE']
-		yieldAmount = capitalPlusYield - capital
-		df.loc[i, YIELD] = yieldAmount
-		if i < lastRowIdx:
-			capitalBefore = df.loc[i + 1, CAPITAL]
-			df.loc[i + 1, CAPITAL] = capitalBefore + capitalPlusYield
+		currentDepWithdr = df.loc[i, DEPWITHDR]
+		if currentDepWithdr >= 0:
+			currentCapital = df.loc[i, CAPITAL] + currentDepWithdr
+			df.loc[i, CAPITAL] = currentCapital
+			capitalPlusYield = currentCapital * df.loc[i, 'RATE']
+			yieldAmount = capitalPlusYield - currentCapital
+			df.loc[i, YIELD] = yieldAmount
+			if i < lastRowIdx:
+				df.loc[i + 1, CAPITAL] = capitalPlusYield
+		else:
+			currentCapital = df.loc[i, CAPITAL]
+			capitalPlusYield = currentCapital * df.loc[i, 'RATE']
+			yieldAmount = capitalPlusYield - currentCapital
+			df.loc[i, YIELD] = yieldAmount
+			if i < lastRowIdx:
+				# in case of withdrawal, the witdrawn amount continues to generate revenue
+				# during the withdrawal date ! So, the capital amount minus the withdrawal
+				# amount is set to the next day (next row).
+				df.loc[i + 1, CAPITAL] = capitalPlusYield + currentDepWithdr
 
 	df.loc[TOTAL] = df.sum(numeric_only=True, axis=0)[
 		[YIELD]]
@@ -42,7 +58,7 @@ randomYearlyInterestRates = np.random.uniform(low=1.15, high=1.25, size=(dayNumb
 # 365th root of a yearly interest rate
 randomDailyInterestRates = np.power(randomYearlyInterestRates, 1/365)
 
-dfRandom = pd.DataFrame({DATE: dayDates, CAPITAL: capitalArray, RATE: randomDailyInterestRates, YIELD: zeroYieldArray})
+dfRandom = pd.DataFrame({DATE: dayDates, DEPWITHDR: depWithdrArray, CAPITAL: capitalArray, RATE: randomDailyInterestRates, YIELD: zeroYieldArray})
 
 print('Random yields\n')		
 print(computeYields(dfRandom))
