f1d455447fa7d0422562f369504cbfdcff290611
Archanciel <jp.schnyder@gmail.com>
2021-04-10 09:10:51 +0200

    restored getting historical rates

--- pricerequester.py
+++ pricerequester.py
@@ -28,6 +28,143 @@ class PriceRequester:
             #occurs when run in QPython under Python 3.2
             self.ctx = None
 
+    def getHistoricalPriceAtUTCTimeStamp(self,
+                                         crypto,
+                                         unit,
+                                         timeStampLocalForHistoMinute,
+                                         localTz,
+                                         timeStampUTCNoHHMMForHistoDay,
+                                         exchange):
+        '''
+        Why do we pass two different time stamp to the method ?
+
+        When requesting a minute price the time stamp obtained
+        from a local arrow datetime object is used. If we
+        request a minute price for 15:18 Zurich time (+00.02)
+        time, the minute price returned is the one at UTC 13:18.
+        Asking the same price from Mumbay would require to
+        ask the price for 20:18 (+00.05).
+
+        Now, when asking an histo day price, the returned price
+        is a close price. But crypto trade 24 H a day. The "close"
+        price time stamp must not be dependant from the location
+        from which the request is sent. Instead, the "close" price
+        is the one at the passed date with hour and minute set to 0.
+        This date is not a localiszd date, but a UTC localization
+        independent date.
+
+        When you go on the Cryptocompare site and you search for
+        historical prices, the close price for a given date is
+        the same whatever the location of the user is !
+
+        :seqdiag_note Obtainins a minute price if request date < 7 days from now, else a day close price.
+        :seqdiag_return ResultData
+        '''
+        resultData = ResultData()
+
+        resultData.setValue(ResultData.RESULT_KEY_CRYPTO, crypto)
+        resultData.setValue(ResultData.RESULT_KEY_UNIT, unit)
+        resultData.setValue(ResultData.RESULT_KEY_EXCHANGE, exchange)
+
+        if DateTimeUtil.isTimeStampOlderThan(timeStampLocalForHistoMinute, localTz, dayNumberInt=MINUTE_PRICE_DAY_NUMBER_LIMIT):
+            return self._getHistoDayPriceAtUTCTimeStamp(crypto, unit, timeStampUTCNoHHMMForHistoDay, exchange, resultData)
+        else:
+            return self._getHistoMinutePriceAtUTCTimeStamp(crypto, unit, timeStampLocalForHistoMinute, exchange, resultData)
+
+
+    def _getHistoMinutePriceAtUTCTimeStamp(self, crypto, unit, timeStampUTC, exchange, resultData):
+        timeStampUTCStr = str(timeStampUTC)
+        url = "https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit=1&aggregate=1&toTs={}&e={}".format(crypto, unit, timeStampUTCStr, exchange)
+        resultData.setValue(ResultData.RESULT_KEY_PRICE_TYPE, resultData.PRICE_TYPE_HISTO_MINUTE)
+
+        try:
+            if self.ctx == None:
+                #here, run in QPython under Python 3.2
+                webURL = urllib.request.urlopen(url)
+            else:
+                webURL = urllib.request.urlopen(url, context=self.ctx)
+        except HTTPError as e:
+            resultData.setError('ERROR - could not complete request ' + url + '. Check your internet connection. Details: ' + str(e.reason) + '.')
+        except URLError as e:
+            resultData.setError('ERROR - No internet. Please connect and retry !')
+        except:
+            the_type, the_value, the_traceback = sys.exc_info()
+            resultData.setError('ERROR - could not complete request ' + url + '. Reason: ' + str(the_type) + '.')
+        else:
+            page = webURL.read()
+            soup = BeautifulSoup(page, 'html.parser')
+            dic = json.loads(soup.prettify())
+            dataListOrDic = dic['Data']
+            if dataListOrDic != []:
+                try:
+                    dataEntryDic = dataListOrDic[IDX_DATA_ENTRY_TO]
+                    resultData.setValue(ResultData.RESULT_KEY_PRICE_TIME_STAMP, dataEntryDic['time'])
+                    resultData.setValue(ResultData.RESULT_KEY_PRICE, dataEntryDic['close'])
+#                except IndexError: # does not happen in any test case
+#                    resultData = self._handleProviderError(dic, resultData, url, crypto, unit, exchange, isRealTime=False)
+                except KeyError:
+                    # happens when pair coupled to exchange do not return ay data.
+                    # Either the exchange does not exist or the pair is not
+                    # supported by the exchange.
+                    resultData = self._handleProviderError(dic, resultData, url, crypto, unit, exchange, isRealTime=False)
+            else:
+                resultData = self._handleProviderError(dic, resultData, url, crypto, unit, exchange, isRealTime=False)
+
+        return resultData
+
+
+    def _getHistoDayPriceAtUTCTimeStamp(self, crypto, unit, timeStampUTC, exchange, resultData):
+        '''
+
+        :param crypto:
+        :param unit:
+        :param timeStampUTC:
+        :param exchange:
+        :param resultData:
+
+        :return: resultData
+        '''
+        timeStampUTCStr = str(timeStampUTC)
+        url = "https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}&limit=1&aggregate=1&toTs={}&e={}".format(crypto, unit, timeStampUTCStr, exchange)
+        resultData.setValue(ResultData.RESULT_KEY_PRICE_TYPE, resultData.PRICE_TYPE_HISTO_DAY)
+
+        try:
+            if self.ctx == None:
+                #here, run in QPython under Python 3.2
+                webURL = urllib.request.urlopen(url)
+            else:
+                webURL = urllib.request.urlopen(url, context=self.ctx)
+        except HTTPError as e:
+            resultData.setError('ERROR - could not complete request ' + url + '. Reason: ' + str(e.reason) + '.')
+        except URLError as e:
+            resultData.setError('ERROR - No internet. Please connect and retry !')
+        except:
+            the_type, the_value, the_traceback = sys.exc_info()
+            resultData.setError('ERROR - could not complete request ' + url + '. Reason: ' + str(the_type) + '.')
+        else:
+            page = webURL.read()
+            soup = BeautifulSoup(page, 'html.parser')
+            dic = json.loads(soup.prettify())
+#            if dic['Data'] != [] and unit in dic:
+            dataListOrDic = dic['Data']
+            if dataListOrDic != []:
+                try:
+                    dataEntryDic = dataListOrDic[IDX_DATA_ENTRY_TO]
+                    resultData.setValue(ResultData.RESULT_KEY_PRICE_TIME_STAMP, dataEntryDic['time'])
+                    resultData.setValue(ResultData.RESULT_KEY_PRICE, dataEntryDic['close'])
+                except IndexError: # does not happen in any test case
+                    resultData = self._handleProviderError(dic, resultData, url, crypto, unit, exchange,isRealTime=False)
+                except KeyError:
+                    # happens when pair coupled to exchange do not return ay data.
+                    # Either the exchange does not exist or the pair is not
+                    # supported by the exchange.
+                    resultData = self._handleProviderError(dic, resultData, url, crypto, unit, exchange, isRealTime=False)
+            else:
+                resultData = self._handleProviderError(dic, resultData, url, crypto, unit, exchange, isRealTime=False)
+
+        return resultData
+
+
     def getCurrentPrice(self, crypto, unit, exchange):
         url = "https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}&e={}".format(crypto, unit, exchange)
         resultData = ResultData()
--- test/testpricerequester.py
+++ test/testpricerequester.py
@@ -7,6 +7,7 @@ sys.path.insert(0, parentdir)
 
 from pricerequester import PriceRequester
 from resultdata import ResultData
+from datetimeutil import DateTimeUtil
 
 
 class TestPriceRequester(unittest.TestCase):
@@ -17,6 +18,146 @@ class TestPriceRequester(unittest.TestCase):
 			FILE_PATH = 'c:\\temp\\cryptopricer.ini'
 
 		self.priceRequester = PriceRequester()
+
+
+	def test_getHistoDayPriceAtUTCTimeStampEndOfDay(self):
+		crypto = 'BTC'
+		unit = 'USD'
+		exchange = 'CCCAGG'
+		utcArrowDateTimeObj_endOfDay = DateTimeUtil.dateTimeStringToArrowLocalDate("2017/09/30 23:59:59", 'UTC',
+																				   "YYYY/MM/DD HH:mm:ss")
+		resultData = ResultData()
+		resultData = self.priceRequester._getHistoDayPriceAtUTCTimeStamp(crypto,
+																		  unit,
+																		  utcArrowDateTimeObj_endOfDay.timestamp,
+																		  exchange,
+																		  resultData)
+		self.assertEqual(1506729600, resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP))
+		priceArrowUTCDateTime = DateTimeUtil.timeStampToArrowLocalDate(resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP),
+																	   'UTC')
+		self.assertEqual(resultData.getValue(resultData.RESULT_KEY_PRICE_TYPE), resultData.PRICE_TYPE_HISTO_DAY)
+		self.assertEqual('30/09/17', priceArrowUTCDateTime.format('DD/MM/YY'))
+		self.assertEqual(4360.62, resultData.getValue(resultData.RESULT_KEY_PRICE))
+
+
+	def test_getHistoDayPriceAtUTCTimeStampMidOfDay(self):
+		crypto = 'BTC'
+		unit = 'USD'
+		exchange = 'CCCAGG'
+		utcArrowDateTimeObj_midOfDay = DateTimeUtil.dateTimeStringToArrowLocalDate("2017/09/30 12:59:59", 'UTC',
+																				   "YYYY/MM/DD HH:mm:ss")
+		resultData = ResultData()
+		resultData = self.priceRequester._getHistoDayPriceAtUTCTimeStamp(crypto,
+																		  unit,
+																		  utcArrowDateTimeObj_midOfDay.timestamp,
+																		  exchange,
+																		  resultData)
+		self.assertEqual(1506729600, resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP))
+		priceArrowUTCDateTime = DateTimeUtil.timeStampToArrowLocalDate(resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP),
+																	   'UTC')
+		self.assertEqual(resultData.getValue(resultData.RESULT_KEY_PRICE_TYPE), resultData.PRICE_TYPE_HISTO_DAY)
+		self.assertEqual('30/09/17', priceArrowUTCDateTime.format('DD/MM/YY'))
+		self.assertEqual(4360.62, resultData.getValue(resultData.RESULT_KEY_PRICE))
+
+
+	def testGetHistoricalPriceAtUTCTimeStampEndOfDay(self):
+		crypto = 'BTC'
+		unit = 'USD'
+		exchange = 'CCCAGG'
+		localTimeZone = 'Europe/Zurich'
+		timeStampLocal = DateTimeUtil.dateTimeStringToTimeStamp("2017/09/30 23:59:59", localTimeZone,
+																"YYYY/MM/DD HH:mm:ss")
+		timeStampUtcNoHHMM = DateTimeUtil.dateTimeStringToTimeStamp("2017/09/30 00:00:00", 'UTC',
+																	"YYYY/MM/DD HH:mm:ss")
+		resultData = self.priceRequester.getHistoricalPriceAtUTCTimeStamp(crypto,
+																		  unit,
+																		  timeStampLocal,
+																		  localTimeZone,
+																		  timeStampUtcNoHHMM,
+																		  exchange)
+		self.assertEqual(1506729600, resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP))
+		priceArrowUTCDateTime = DateTimeUtil.timeStampToArrowLocalDate(resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP),
+																	   'UTC')
+		self.assertEqual(resultData.getValue(resultData.RESULT_KEY_PRICE_TYPE), resultData.PRICE_TYPE_HISTO_DAY)
+		self.assertEqual('30/09/17', priceArrowUTCDateTime.format('DD/MM/YY'))
+		self.assertEqual(4360.62, resultData.getValue(resultData.RESULT_KEY_PRICE))
+		self.assertEqual(crypto, resultData.getValue(resultData.RESULT_KEY_CRYPTO))
+		self.assertEqual(unit, resultData.getValue(resultData.RESULT_KEY_UNIT))
+		self.assertEqual(exchange, resultData.getValue(resultData.RESULT_KEY_EXCHANGE))
+
+
+	def testGetHistoricalPriceAtUTCTimeStampMidOfDay(self):
+		crypto = 'BTC'
+		unit = 'USD'
+		exchange = 'CCCAGG'
+		localTimeZone = 'Europe/Zurich'
+
+		timeStampLocal = DateTimeUtil.dateTimeStringToTimeStamp("2017/09/30 12:59:59", localTimeZone,
+																"YYYY/MM/DD HH:mm:ss")
+		timeStampUtcNoHHMM = DateTimeUtil.dateTimeStringToTimeStamp("2017/09/30 00:00:00", 'UTC',
+																	"YYYY/MM/DD HH:mm:ss")
+		resultData = self.priceRequester.getHistoricalPriceAtUTCTimeStamp(crypto,
+																		  unit,
+																		  timeStampLocal,
+																		  localTimeZone,
+																		  timeStampUtcNoHHMM,
+																		  exchange)
+
+		self.assertEqual(1506729600, resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP))
+		priceArrowUTCDateTime = DateTimeUtil.timeStampToArrowLocalDate(resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP),
+																	   'UTC')
+		self.assertEqual(resultData.getValue(resultData.RESULT_KEY_PRICE_TYPE), resultData.PRICE_TYPE_HISTO_DAY)
+		self.assertEqual('30/09/17', priceArrowUTCDateTime.format('DD/MM/YY'))
+		self.assertEqual(4360.62, resultData.getValue(resultData.RESULT_KEY_PRICE))
+		self.assertEqual(crypto, resultData.getValue(resultData.RESULT_KEY_CRYPTO))
+		self.assertEqual(unit, resultData.getValue(resultData.RESULT_KEY_UNIT))
+		self.assertEqual(exchange, resultData.getValue(resultData.RESULT_KEY_EXCHANGE))
+
+
+	def testGetHistoricalPriceAtTimeStampZurichMidOfDayUseTimeStamp(self):
+		crypto = 'BTC'
+		unit = 'USD'
+		exchange = 'CCCAGG'
+
+		#time stamp is always UTC !
+		timeStampLocalMidDay = DateTimeUtil.dateTimeStringToTimeStamp("2017/09/30 12:59:59", 'Europe/Zurich',
+																"YYYY/MM/DD HH:mm:ss")
+		timeStampUtcNoHHMM = DateTimeUtil.dateTimeStringToTimeStamp("2017/09/30 00:00:00", 'UTC',
+																	"YYYY/MM/DD HH:mm:ss")
+		resultData = self.priceRequester.getHistoricalPriceAtUTCTimeStamp(crypto, unit,
+																		  timeStampLocalMidDay,
+																		  'Europe/Zurich',
+																		  timeStampUtcNoHHMM,
+																		  exchange)
+		self.assertEqual(1506729600, resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP))
+		priceArrowUTCDateTime = DateTimeUtil.timeStampToArrowLocalDate(resultData.getValue(resultData.RESULT_KEY_PRICE_TIME_STAMP),
+																	   'UTC')
+		self.assertEqual(resultData.getValue(resultData.RESULT_KEY_PRICE_TYPE), resultData.PRICE_TYPE_HISTO_DAY)
+		self.assertEqual('30/09/17', priceArrowUTCDateTime.format('DD/MM/YY'))
+		self.assertEqual(4360.62, resultData.getValue(resultData.RESULT_KEY_PRICE))
+		self.assertEqual(crypto, resultData.getValue(resultData.RESULT_KEY_CRYPTO))
+		self.assertEqual(unit, resultData.getValue(resultData.RESULT_KEY_UNIT))
+		self.assertEqual(exchange, resultData.getValue(resultData.RESULT_KEY_EXCHANGE))
+
+
+	def testGetHistoricalPriceAtUTCTimeStampLessThanSevenDay(self):
+		crypto = 'BTC'
+		unit = 'USD'
+		exchange = 'CCCAGG'
+		utcArrowDateTimeObj = DateTimeUtil.localNow('UTC')
+		utcArrowDateTimeObj = utcArrowDateTimeObj.shift(days=-2)
+
+		# for histominute price,
+		resultData = self.priceRequester.getHistoricalPriceAtUTCTimeStamp(crypto,
+																		  unit,
+																		  timeStampLocalForHistoMinute=utcArrowDateTimeObj.timestamp,
+																		  localTz=None,
+																		  timeStampUTCNoHHMMForHistoDay=utcArrowDateTimeObj.timestamp,
+																		  exchange=exchange)
+		self.assertEqual(resultData.getValue(resultData.RESULT_KEY_PRICE_TYPE), resultData.PRICE_TYPE_HISTO_MINUTE)
+		self.assertEqual(crypto, resultData.getValue(resultData.RESULT_KEY_CRYPTO))
+		self.assertEqual(unit, resultData.getValue(resultData.RESULT_KEY_UNIT))
+		self.assertEqual(exchange, resultData.getValue(resultData.RESULT_KEY_EXCHANGE))
 
 
 	def testGetCurrentPrice(self):
