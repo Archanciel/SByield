2029b83d7b5800a61711ac059ac739ad2960e918
Archanciel <jp.schnyder@gmail.com>
2021-04-23 05:59:43 +0200

    Create alltests.py

---
+++ test/alltests.py
@@ -0,0 +1,32 @@
+'''
+This test suite runs on Pydroid 3, but not on QPython 3 since it has a dependency on
+Kivy resources which are not supported by QPython 3. It has no dependency on the sl4a
+library, supported by QPython 3, but not by Pydroid 3. It can be executed in Pycharm on
+pc !
+'''
+
+from unittest import TestLoader, TextTestRunner, TestSuite
+
+from testpricerequester import TestPriceRequester
+from testcryptofiatratecomputer import TestCryptoFiatRateComputer
+from testsbyieldratecomputer import TestSBYieldRateComputer
+from testownerdeposityieldcomputer import TestOwnerDepositYieldComputer
+from testprocessor import TestProcessor
+from testcontroller import TestController
+
+
+if __name__ == "__main__":
+    '''
+    This test suite runs on Android in Pydroid, but fails in QPython !
+    '''
+    loader = TestLoader()
+    suite = TestSuite((loader.loadTestsFromTestCase(TestPriceRequester),
+                       loader.loadTestsFromTestCase(TestCryptoFiatRateComputer),
+                       loader.loadTestsFromTestCase(TestCryptoFiatRateComputer),
+                       loader.loadTestsFromTestCase(TestSBYieldRateComputer),
+                       loader.loadTestsFromTestCase(TestOwnerDepositYieldComputer),
+                       loader.loadTestsFromTestCase(TestProcessor),
+                       loader.loadTestsFromTestCase(TestController)
+    ))
+    runner = TextTestRunner(verbosity = 2)
+    runner.run(suite)
